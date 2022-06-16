5223e4b47964b8fcbf1adaa96df0ef76dcb9b539
isacpxc <isacpxc@gmail.com>
2022-06-16 15:41:58 -0300

    ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from 'react-dom';
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
-}
-
-setInterval(tick, 1000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
